72238feade38f929e104eecdb30c48004ccbd983
Ann Wolff <annwolffe@gmail.com>
2017-04-17 17:04:26 -0600

    Remove extra body parser

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const cors = require('cors')
 var pug = require('pug');
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
 app.set('view engine', 'pug');
